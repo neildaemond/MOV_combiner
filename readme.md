30f0fa27dc4350b76e26bff0e234c07ff7c4efdb
Neil Pahl <neildaemond@gmail.com>
2017-06-19 21:35:09 +0800

    added rotation tip

--- readme.md
+++ readme.md
@@ -11,9 +11,18 @@
 
 videos should be concatenated in alphabetical order
 
+** **Note:** Intermediate videos are created then deleted in an insecure manner - ie: the videos will probably show up during any file recovery effort. Therefore, one should not use this tool with sensitive videos. 
+
 # Tips
 
-You can rotate a video with `ffmpeg -i in.MOV -vf "transpose=1" out.mpg`
+You can rotate a (1920x1080) video with
+
+        ffmpeg -i in.MOV -vf "transpose=1" out.mpg
+
+or 
+
+        ffmpeg -noautorotate -i out.MOV -strict -2 -qscale:v 1 -vf "transpose=2,scale=-1:1080,pad=1920:1080:656:0" out.MOV
+
 
 For the transpose parameter you can pass:
 
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ i=0
 for f in `ls ./combine_queue/* | sort -g `; do 
     filename=$(basename "$f")
     filename="${filename%.*}"
-    ffmpeg -i $f -qscale:v 1 ./combined/int_$filename.mpg
+    ffmpeg -i $f -qscale:v 0 ./combined/int_$filename.mpg
     if (( $i == 0 )); then
         concatstring="./combined/int_$filename.mpg"
     else
@@ -14,6 +14,7 @@ for f in `ls ./combine_queue/* | sort -g `; do
 done;
 
 ffmpeg -i concat:"$concatstring" -c copy ./combined/combined_$1.mpg
-ffmpeg -i ./combined/combined_$1.mpg -qscale:v 2 ./combined/combined_$1.avi
+#ffmpeg -i ./combined/combined_$1.mpg -qscale:v 2 ./combined/combined_$1.avi
 
-rm -rf ./combined/int_*.mpg
+mv  ./combine_queue/* ./combine_used/
+rm  ./combined/int_*.mpg
